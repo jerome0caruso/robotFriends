f9c59cb617ce819242e2fc5c69cd4f8c906d54dc
jerome0caruso <jerome0caruso@myjavascriptprojects.com>
2020-09-07 19:18:33 -0500

    Updates

---
+++ precache-manifest.0affdb9f21f123f443cc6d5d228e1d8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f436379705a328f7fe98ac46c1dad7",
+    "url": "/robotFriends/index.html"
+  },
+  {
+    "revision": "a649fd38ba8569e19384",
+    "url": "/robotFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e1d60cbc395993523752",
+    "url": "/robotFriends/static/css/main.7f8663d5.chunk.css"
+  },
+  {
+    "revision": "a649fd38ba8569e19384",
+    "url": "/robotFriends/static/js/2.907bdc06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotFriends/static/js/2.907bdc06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d60cbc395993523752",
+    "url": "/robotFriends/static/js/main.35ca0079.chunk.js"
+  },
+  {
+    "revision": "6e28b547d8cfa8bf0f5c",
+    "url": "/robotFriends/static/js/runtime-main.1566e6c0.js"
+  }
+]);
